20f3b97948d643eef72efb84055caf77b9360fa7
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-10-27 18:09:06 -0300

    Update data class

--- back/23650-tp-final/src/main/java/com/ar/cac/tpfinal/services/UserService.java
+++ back/23650-tp-final/src/main/java/com/ar/cac/tpfinal/services/UserService.java
@@ -5,9 +5,9 @@ import com.ar.cac.tpfinal.entities.dtos.UserDto;
 import com.ar.cac.tpfinal.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
 @Service
