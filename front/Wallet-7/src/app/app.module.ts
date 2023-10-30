21d98c7e13f0c52a72a617a820c1c080f067e0ab
exequiel <conan34681335@gmail.com>
2023-10-30 16:01:34 -0300

    preparando Angular

--- front/Wallet-7/src/app/app.module.ts
+++ front/Wallet-7/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { PerfilComponent } from './Components/perfil/perfil.component';
+import { NavBarComponent } from './ComponentesReutilizables/nav-bar/nav-bar.component';
+import { FormComponent } from './ComponentesReutilizables/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeComponent,
+    PerfilComponent,
+    NavBarComponent,
+    FormComponent,
+     ],
   imports: [
     BrowserModule
   ],
